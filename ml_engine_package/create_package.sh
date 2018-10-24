f1ab22dab6143ac4de0b9b41e04c65e58cdf5754
Balazs Toth <tothbalazs0920@gmail.com>
2018-10-24 10:50:50 +0200

    Rename package

--- ml_engine_package/create_package.sh
+++ ml_engine_package/create_package.sh
@@ -8,6 +8,6 @@ rm -rf sentiment_analyzer.egg-info
 # Build the distribution
 python setup.py sdist --formats=gztar
 # Upload the distribution to GCS
-gsutil cp dist/sentiment-analyzer-0.1.tar.gz gs://20-percent-mlengineexample/package/latest.tar.gz
+gsutil cp dist/newsgroupclassification-0.1.tar.gz gs://20-percent-mlengineexample/package/latest.tar.gz
 
 echo "Distribution uploaded"
